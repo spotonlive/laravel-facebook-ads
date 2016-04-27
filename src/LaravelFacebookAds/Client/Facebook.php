0822842df53966fca7ef37137ef630bfaee7a1b5
Nikolaj Petersen <nikolaj.lovenhardt@gmail.com>
2016-04-27 11:28:48 +0200

    Facebook client

---
+++ src/LaravelFacebookAds/Client/Facebook.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace LaravelFacebookAds\Client;
+
+use FacebookAds\Cursor;
+use FacebookAds\Object\Ad;
+use FacebookAds\Object\AdAccount;
+use FacebookAds\Object\AdPreview;
+use FacebookAds\Object\Campaign;
+use FacebookAds\Object\Fields\AdPreviewFields;
+use LaravelFacebookAds\Exceptions\NoAccountSelectedException;
+use LaravelFacebookAds\Services\FacebookAdsServiceInterface;
+
+/**
+ * Class Facebook
+ * @package LaravelFacebookAds\Client
+ */
+class Facebook
+{
+    /** @var FacebookAdsServiceInterface */
+    protected $facebookAdsService;
+
+    /** @var string|null */
+    protected $accountId = null;
+
+    /**
+     * Facebook constructor.
+     * @param FacebookAdsServiceInterface $facebookAdsService
+     */
+    public function __construct(FacebookAdsServiceInterface $facebookAdsService)
+    {
+        $this->facebookAdsService = $facebookAdsService;
+    }
+
+    /**
+     * Set account
+     *
+     * @param string $id
+     * @return $this
+     */
+    public function account($id)
+    {
+        $this->accountId = $id;
+        return $this;
+    }
+
+    /**
+     * Get ads
+     * This method requires that an ad account has been set
+     *
+     * @param array $fields
+     * @param array $params
+     * @return Ad[]|array|Cursor
+     * @throws \Exception
+     */
+    public function ads($fields = [], $params = [])
+    {
+        $account = $this->getAccount();
+
+        return $account->getAds($fields, $params);
+    }
+
+    /**
+     * Get campaigns
+     * This method requires that an ad account has been set
+     *
+     * @param array $fields
+     * @param array $params
+     * @return Cursor|Campaign[]|array
+     * @throws \Exception
+     */
+    public function campaigns($fields = [], $params = [])
+    {
+        $account = $this->getAccount();
+
+        return $account->getCampaigns($fields, $params);
+    }
+
+    /**
+     * Get ad previews
+     *
+     * @param Ad $ad
+     * @param string $format
+     * @param array $fields
+     * @param array $params
+     * @return Cursor|AdPreview[]|array
+     */
+    public function adPreviews(Ad $ad, $format, $fields = [], $params = [])
+    {
+        // Merge
+        $params = array_merge([
+            AdPreviewFields::AD_FORMAT => $format,
+        ], $params);
+
+        return $ad->getAdPreviews($fields, $params);
+    }
+
+    /**
+     * Get facebook ad account
+     *
+     * @return AdAccount
+     * @throws NoAccountSelectedException
+     */
+    public function getAccount()
+    {
+        if (is_null($this->accountId)) {
+            throw new NoAccountSelectedException();
+        }
+
+        return new AdAccount($this->accountId);
+    }
+}
---
+++ src/LaravelFacebookAds/Exceptions/NoAccountSelectedException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LaravelFacebookAds\Exceptions;
+
+class NoAccountSelectedException extends Exception
+{
+    /**
+     * No account selected exception
+     *
+     * @param int $code
+     * @param \Exception|null $previous
+     */
+    public function __construct($code = 0, \Exception $previous = null)
+    {
+        parent::__construct('No account was selected', $code, $previous);
+    }
+}
